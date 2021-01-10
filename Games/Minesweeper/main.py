a1604688d47dbbc9940728acad03cc97b49a1973
sanderwl <31347023+sanderwl@users.noreply.github.com>
2021-01-10 15:18:15 +0100

    e
    e

--- Games/Minesweeper/main.py
+++ Games/Minesweeper/main.py
@@ -2,9 +2,9 @@ import numpy as np
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 
-num1 = 30
-num2 = 16
-points = num1*num2
+rows = 16
+columns = 30
+points = rows*columns
 
 driver = webdriver.Chrome()
 driver.get("http://minesweeperonline.com/#200")
@@ -13,18 +13,25 @@ startClick = driver.find_element_by_xpath('//*[@id="9_14"]')
 actions = ActionChains(driver)
 actions.click(startClick).perform()
 
-#squareType = print(startClick.get_attribute("class")[:11])
-#squareDigit = print(startClick.get_attribute("class")[-1])
-#squareID = print(startClick.get_attribute("id"))
+squareType = print(startClick.get_attribute("class")[:11])
+squareDigit = print(startClick.get_attribute("class")[-1])
+squareID = print(startClick.get_attribute("id"))
 
-gridMat = np.zeros((num2,num1))
-print(str(7) + "_" + str(8))
+gridMat = np.zeros((rows, columns))
+print(gridMat.shape)
 
 def grid():
-    for i in range(1, num1+1):
-        for j in range(1, num2+1):
-            xpath = "//*[@id='" + str(j) + "_" + str(i) + "')]"
-            gridMat[j, i] = driver.find_element_by_xpath(xpath)
+    for i in range(1, rows+1):
+        for j in range(1, columns+1):
+            xpath = "//*[@id='" + str(i) + "_" + str(j) + "']"
+            current = driver.find_element_by_xpath(xpath)
+            if (isinstance(current.get_attribute("class")[-1], int) == True):
+                gridMat[i-1, j-1] = current.get_attribute("class")[-1]
+            else:
+                gridMat[i-1, j-1] = 99
     return gridMat
+
 gridMat = grid()
 print(gridMat)
+
+#//*[@id="1_1"]
