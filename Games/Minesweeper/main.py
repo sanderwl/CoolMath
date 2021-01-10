cc8184d54b9b50b5929458a2706593a880183e84
sanderwl <31347023+sanderwl@users.noreply.github.com>
2021-01-10 22:15:23 +0100

    e
    e

--- Games/Minesweeper/main.py
+++ Games/Minesweeper/main.py
@@ -2,36 +2,85 @@ import numpy as np
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 
-rows = 16
-columns = 30
-points = rows*columns
+num1 = 30
+num2 = 16
+points = num1*num2
+startID = "9_14"
 
 driver = webdriver.Chrome()
 driver.get("http://minesweeperonline.com/#200")
 
-startClick = driver.find_element_by_xpath('//*[@id="9_14"]')
+startClick = driver.find_element_by_xpath('//*[@id="' + startID + '"]')
 actions = ActionChains(driver)
 actions.click(startClick).perform()
 
-squareType = print(startClick.get_attribute("class")[:11])
-squareDigit = print(startClick.get_attribute("class")[-1])
-squareID = print(startClick.get_attribute("id"))
+startNum = startID.find("_")
+currentID = startID
+currentColumn = int(currentID[startNum + 1:])
+currentRow = int(currentID[:startNum])
+currentClick = driver.find_element_by_xpath('//*[@id="' + currentID + '"]')
+currentNum = 0
 
-gridMat = np.zeros((rows, columns))
-print(gridMat.shape)
+while (int(currentClick.get_attribute("class")[-1]) < 1):
+    currentRow -= 1
+    currentID = str(currentRow) + "_" + str(currentColumn)
+    currentClick = driver.find_element_by_xpath('//*[@id="' + currentID + '"]')
+    currentNum = currentClick.get_attribute("class")[-1]
+    print(currentClick.get_attribute("class")[-1])
+    print(currentID)
 
-def grid():
-    for i in range(1, rows+1):
-        for j in range(1, columns+1):
-            xpath = "//*[@id='" + str(i) + "_" + str(j) + "']"
-            current = driver.find_element_by_xpath(xpath)
-            if (isinstance(current.get_attribute("class")[-1], int) == True):
-                gridMat[i-1, j-1] = current.get_attribute("class")[-1]
-            else:
-                gridMat[i-1, j-1] = 99
-    return gridMat
+squareGrid = np.zeros((3,3))
 
-gridMat = grid()
-print(gridMat)
+def surrounding(currentNum):
 
-#//*[@id="1_1"]
+    count = 0
+    upID = str(currentRow-1) + "_" + str(currentColumn)
+    surroundClickUp = driver.find_element_by_xpath('//*[@id="' + upID + '"]')
+    if (surroundClickUp.get_attribute("class")[:12] == "square blank"):
+        count += 1
+
+    upLeftId = str(currentRow-1) + "_" + str(currentColumn-1)
+    surroundClickUpLeft = driver.find_element_by_xpath('//*[@id="' + upLeftId + '"]')
+    if (surroundClickUpLeft.get_attribute("class")[:12] == "square blank"):
+        count += 1
+
+    leftID = str(currentRow) + "_" + str(currentColumn-1)
+    surroundClickLeft = driver.find_element_by_xpath('//*[@id="' + leftID + '"]')
+    if (surroundClickLeft.get_attribute("class")[:12] == "square blank"):
+        count += 1
+
+    downLeftID = str(currentRow+1) + "_" + str(currentColumn-1)
+    surroundClickDownLeft = driver.find_element_by_xpath('//*[@id="' + downLeftID + '"]')
+    if (surroundClickDownLeft.get_attribute("class")[:12] == "square blank"):
+        count += 1
+
+    downID = str(currentRow+1) + "_" + str(currentColumn)
+    surroundClickDown = driver.find_element_by_xpath('//*[@id="' + downID + '"]')
+    if (surroundClickDown.get_attribute("class")[:12] == "square blank"):
+        count += 1
+
+    downRightID = str(currentRow+1) + "_" + str(currentColumn+1)
+    surroundClickDownRight = driver.find_element_by_xpath('//*[@id="' + downRightID + '"]')
+    if (surroundClickDownRight.get_attribute("class")[:12] == "square blank"):
+        count += 1
+
+    rightID = str(currentRow) + "_" + str(currentColumn+1)
+    surroundClickRight = driver.find_element_by_xpath('//*[@id="' + rightID + '"]')
+    if (surroundClickRight.get_attribute("class")[:12] == "square blank"):
+        count += 1
+
+    upRightID = str(currentRow-1) + "_" + str(currentColumn+1)
+    surroundClickUpRight = driver.find_element_by_xpath('//*[@id="' + upRightID + '"]')
+    if (surroundClickUpRight.get_attribute("class")[:12] == "square blank"):
+        count += 1
+
+    if (currentNum == count):
+        actions.context_click(surroundClick).perform()
+    return count
+
+
+
+#squareType = print(startClick.get_attribute("class")[:11])
+#squareDigit = print(startClick.get_attribute("class")[-1])
+#squareID = print(startClick.get_attribute("id"))
+
