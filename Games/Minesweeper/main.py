b1902ef9c51c366fe255eec1cd3701bb36ecf0c4
sanderwl <31347023+sanderwl@users.noreply.github.com>
2021-01-11 19:55:55 +0100

    e
    e

--- Games/Minesweeper/main.py
+++ Games/Minesweeper/main.py
@@ -2,10 +2,12 @@ import numpy as np
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 
-num1 = 30
-num2 = 16
-points = num1*num2
-startID = "9_14"
+columns = 30
+rows = 16
+points = columns * rows
+startrow = 9
+startColumn = 14
+startID = str(startrow) + "_" + str(startColumn)
 
 driver = webdriver.Chrome()
 driver.get("http://minesweeperonline.com/#200")
@@ -14,69 +16,20 @@ startClick = driver.find_element_by_xpath('//*[@id="' + startID + '"]')
 actions = ActionChains(driver)
 actions.click(startClick).perform()
 
-startNum = startID.find("_")
-currentID = startID
-currentColumn = int(currentID[startNum + 1:])
-currentRow = int(currentID[:startNum])
-currentClick = driver.find_element_by_xpath('//*[@id="' + currentID + '"]')
-currentNum = 0
+board = np.zeros((rows, columns))
+grid = board
+for i in range(1, rows+1):
+    for j in range(1, columns+1):
+        IDS = str(i) + "_" + str(j)
+        currentID = driver.find_element_by_xpath('//*[@id="' + IDS + '"]')
+        if (currentID.get_attribute("class")[:12] == "square blank"):
+            board[i - 1][j - 1] = float("NaN")
+        else:
+            board[i - 1][j - 1] = int(currentID.get_attribute("class")[-1])
 
-while (int(currentClick.get_attribute("class")[-1]) < 1):
-    currentRow -= 1
-    currentID = str(currentRow) + "_" + str(currentColumn)
-    currentClick = driver.find_element_by_xpath('//*[@id="' + currentID + '"]')
-    currentNum = currentClick.get_attribute("class")[-1]
-    print(currentClick.get_attribute("class")[-1])
-    print(currentID)
 
-squareGrid = np.zeros((3,3))
 
-def surrounding(currentNum):
 
-    count = 0
-    upID = str(currentRow-1) + "_" + str(currentColumn)
-    surroundClickUp = driver.find_element_by_xpath('//*[@id="' + upID + '"]')
-    if (surroundClickUp.get_attribute("class")[:12] == "square blank"):
-        count += 1
-
-    upLeftId = str(currentRow-1) + "_" + str(currentColumn-1)
-    surroundClickUpLeft = driver.find_element_by_xpath('//*[@id="' + upLeftId + '"]')
-    if (surroundClickUpLeft.get_attribute("class")[:12] == "square blank"):
-        count += 1
-
-    leftID = str(currentRow) + "_" + str(currentColumn-1)
-    surroundClickLeft = driver.find_element_by_xpath('//*[@id="' + leftID + '"]')
-    if (surroundClickLeft.get_attribute("class")[:12] == "square blank"):
-        count += 1
-
-    downLeftID = str(currentRow+1) + "_" + str(currentColumn-1)
-    surroundClickDownLeft = driver.find_element_by_xpath('//*[@id="' + downLeftID + '"]')
-    if (surroundClickDownLeft.get_attribute("class")[:12] == "square blank"):
-        count += 1
-
-    downID = str(currentRow+1) + "_" + str(currentColumn)
-    surroundClickDown = driver.find_element_by_xpath('//*[@id="' + downID + '"]')
-    if (surroundClickDown.get_attribute("class")[:12] == "square blank"):
-        count += 1
-
-    downRightID = str(currentRow+1) + "_" + str(currentColumn+1)
-    surroundClickDownRight = driver.find_element_by_xpath('//*[@id="' + downRightID + '"]')
-    if (surroundClickDownRight.get_attribute("class")[:12] == "square blank"):
-        count += 1
-
-    rightID = str(currentRow) + "_" + str(currentColumn+1)
-    surroundClickRight = driver.find_element_by_xpath('//*[@id="' + rightID + '"]')
-    if (surroundClickRight.get_attribute("class")[:12] == "square blank"):
-        count += 1
-
-    upRightID = str(currentRow-1) + "_" + str(currentColumn+1)
-    surroundClickUpRight = driver.find_element_by_xpath('//*[@id="' + upRightID + '"]')
-    if (surroundClickUpRight.get_attribute("class")[:12] == "square blank"):
-        count += 1
-
-    if (currentNum == count):
-        actions.context_click(surroundClick).perform()
-    return count
 
 
 
